894ac69703ded4bb331dd88a67e3675210beac9b
Scott <scott.m.king@outlook.com>
2022-12-05 22:09:16 -0800

    Day 6

---
+++ solutions/6/1.rs
@@ -0,0 +1,27 @@
+// https://adventofcode.com/2022/day/6
+
+use std::{collections::HashSet, fs};
+
+fn main() {
+    let data = fs::read_to_string("data.txt").unwrap();
+    let chars: Vec<char> = data.chars().collect();
+
+    let mut set: HashSet<char> = HashSet::new();
+
+    let mut i = 1;
+    let mut left = 0;
+
+    for char in chars.as_slice() {
+        while set.contains(&char) {
+            set.remove(&chars[left]);
+            left += 1;
+        }
+        set.insert(*char);
+        if set.len() == 4 {
+            break;
+        }
+        i += 1;
+    }
+
+    println!("{}", i);
+}
