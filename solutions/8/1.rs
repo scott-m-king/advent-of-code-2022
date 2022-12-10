2fc84004f6c7be9dd58cb815a459abaef55c1d93
Scott <scott.m.king@outlook.com>
2022-12-10 12:15:10 -0800

    Refactor again

--- solutions/8/1.rs
+++ solutions/8/1.rs
@@ -16,32 +16,36 @@ fn main() {
 
     let grid_ref = &grid;
 
-    let result = grid
+    let result: i32 = grid
         .iter()
         .enumerate()
         .map(|(row_i, row)| {
             row.iter()
                 .enumerate()
-                .map(move |(col_i, height)| {
-                    if row_i == 0 || col_i == 0 || row_i == grid_ref.len() || col_i == row.len() - 1
+                .filter(move |(col_i, height)| {
+                    if row_i == 0
+                        || *col_i == 0
+                        || row_i == grid_ref.len()
+                        || *col_i == row.len() - 1
                     {
                         return true;
                     }
 
-                    let col = grid_ref.iter().map(|r| r[col_i]).collect::<Vec<i32>>();
+                    let col = grid_ref.iter().map(|r| r[*col_i]).collect::<Vec<i32>>();
 
                     return [
-                        row.get(..col_i).unwrap(),       // before in row
-                        row.get((col_i + 1)..).unwrap(), // after in row
-                        col.get(..row_i).unwrap(),       // before in col
-                        col.get((row_i + 1)..).unwrap(), // after in col
+                        row.get(..*col_i).unwrap(),       // before in row
+                        row.get((*col_i + 1)..).unwrap(), // after in row
+                        col.get(..row_i).unwrap(),        // before in col
+                        col.get((row_i + 1)..).unwrap(),  // after in col
                     ]
                     .iter()
-                    .any(|view| view.iter().all(|view_height| height > view_height));
+                    .any(|view| view.iter().all(|view_height| *height > view_height));
                 })
-                .filter(|x| *x == true)
+                .fold(0, |acc, _| acc + 1)
+            // .filter(|x| *x == true)
         })
-        .fold(0, |acc, curr| acc + curr.collect::<Vec<bool>>().len());
+        .sum();
 
     println!("{:?}", result);
 }
