2fd31edde1fe55f16d5070ce7fc0a4773075a687
Scott <scott.m.king@outlook.com>
2022-12-10 00:19:53 -0800

    Good riddance day 7

---
+++ solutions/7/2.rs
@@ -0,0 +1,105 @@
+// https://adventofcode.com/2022/day/7
+
+use std::{cmp::Reverse, collections::BinaryHeap, fs};
+
+static TOTAL_SPACE: i32 = 70000000;
+static UNUSED_SPACE: i32 = 30000000;
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    index: usize,
+    parent: usize,
+    size: i32,
+}
+
+fn directory_sum(files: &Vec<File>, directories: &Vec<File>, dir_index: usize) -> i32 {
+    let file_sum: i32 = files
+        .iter()
+        .filter(|file| file.parent == dir_index)
+        .map(|file| file.size)
+        .sum();
+
+    return directories
+        .iter()
+        .filter(|dir| dir.index != 0 && dir.parent == dir_index)
+        .fold(file_sum, |sum, dir| {
+            sum + directory_sum(files, directories, dir.index)
+        });
+}
+
+fn main() {
+    let data = fs::read_to_string("data.txt").unwrap();
+    let commands = data.lines();
+
+    let mut current_index = 0;
+    let mut current_parent: Vec<usize> = Vec::new();
+
+    let mut directories: Vec<File> = Vec::new();
+    let mut files: Vec<File> = Vec::new();
+
+    for command in commands {
+        match command.split(" ").collect::<Vec<&str>>().as_slice() {
+            ["$", "cd", ".."] => {
+                current_index = current_parent.pop().unwrap();
+            }
+            ["$", "cd", "/"] => {
+                directories.push(File {
+                    name: String::from("/"),
+                    index: 0,
+                    parent: 0,
+                    size: 0,
+                });
+            }
+            ["$", "cd", dirname] => {
+                current_parent.push(current_index);
+                current_index = directories
+                    .iter()
+                    .find(|dir| dir.parent == current_index && dir.name == *dirname)
+                    .unwrap()
+                    .index;
+            }
+            ["$", "ls"] => {}
+            ["dir", dirname] => {
+                directories.push(File {
+                    name: String::from(*dirname),
+                    index: directories.len(),
+                    parent: current_index,
+                    size: 0,
+                });
+            }
+            [size, filename] => {
+                files.push(File {
+                    name: String::from(*filename),
+                    parent: current_index,
+                    index: 0,
+                    size: size.parse::<i32>().unwrap(),
+                });
+            }
+            _ => {}
+        }
+    }
+
+    let unsorted = directories
+        .iter()
+        .map(|dir| directory_sum(&files, &directories, dir.index))
+        .collect::<Vec<i32>>();
+
+    let mut sorted = unsorted.iter().fold(BinaryHeap::new(), |mut heap, curr| {
+        heap.push(Reverse(curr));
+        return heap;
+    });
+
+    let used_space = unsorted.first().unwrap();
+    let threshold = UNUSED_SPACE - (TOTAL_SPACE - used_space);
+
+    while !sorted.is_empty() && sorted.peek().unwrap().0 <= &threshold {
+        let result = sorted.pop();
+        if sorted.is_empty() {
+            println!("{}", result.unwrap().0);
+            return;
+        }
+    }
+
+    println!("{:?}", sorted.peek().unwrap().0);
+}
